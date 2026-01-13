46394c7c2279b2e40401f704c8a869095ebd0702
OlgaStarchenko <o9915530@gmail.com>
2026-01-13 10:09:58 +1000

    The App.js component has been initialized.

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,33 @@
-import { Component } from '../core/Component';
-import { Form } from './Form';
-import { List } from './List';
-import { ListItem } from './ListItem';
+import { Component } from "../core/Component";
+import { Form } from "./Form";
+import { List } from "./List";
+import { ListItem } from "./ListItem";
 
 export class App extends Component {
   setup(props) {
-    this.$rootElement = document.createElement('div');
-    this.$rootElement.className = 'app';
+    this.state = {
+      total: 0,
+      donates: [],
+    };
+    this.$rootElement = document.createElement("div");
+    this.$rootElement.className = "app";
 
-    // ...
-    
+    const appTitle = document.createElement("h1");
+    const spanTotal = document.createElement("span");
+    spanTotal.textContent = this.state.total;
+    appTitle.textContent = "Итого $";
+
+    this.$total = spanTotal;
+
+    appTitle.appendChild(spanTotal);
+    this.$rootElement.appendChild(appTitle);
+
     const donateForm = new Form();
     this.$rootElement.appendChild(donateForm.$rootElement);
     const donateList = new List();
     this.$rootElement.appendChild(donateList.$rootElement);
   }
-  
+
   onItemCreate(amount) {
     // ...
   }
