a2a58a8e21752d6beaa3a3e4f1ccc551925ac834
OlgaStarchenko <o9915530@gmail.com>
2026-01-15 16:34:29 +1000

    The List.js component has been identified.

--- src/components/List.js
+++ src/components/List.js
@@ -1,11 +1,18 @@
-import { Component } from '../core/Component';
+import { Component } from "../core/Component";
 
 export class List extends Component {
   setup() {
-    this.$rootElement = document.createElement('div');
-    this.$rootElement.className = 'donates-container';
+    this.$rootElement = document.createElement("div");
+    this.$rootElement.className = "donates-container";
 
-    // ...
+    const listTitle = document.createElement("h2");
+    listTitle.className = "donates-container__title";
+    listTitle.textContent = "Список донатов";
+    const donates = document.createElement("div");
+    donates.classList = "donates-container__donates";
+
+    this.$rootElement.appendChild(listTitle);
+    this.$rootElement.appendChild(donates);
   }
 
   addItem(item) {
